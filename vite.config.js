7e6625bbe513d85d66f8ac71b37a591a5aace5d1
inspire12 <ox4443@naver.com>
2025-10-04 23:19:56 +0900

    gh page 설정

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/animation/',
+  base: '/inspire12-animation/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
