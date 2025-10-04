bcd656ca2220f1e2739349060f2bd29528a6c4fe
inspire12 <ox4443@naver.com>
2025-10-04 23:19:26 +0900

    gh page 설정

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/animation-gallery/',
+  base: '/animation/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
